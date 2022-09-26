e91d1537bc05ed4bd54dcd80ffca5a4ec700b83d
julisejnohova <julie.sejnohova@gmail.com>
2022-09-26 16:58:06 +0200

    zacatek

---
+++ index.js
@@ -0,0 +1,26 @@
+const array = ["Lodash", "React", "Next", "Strapi", "Axios", "Typescript"]
+
+// array.forEach(function (item) {
+//     console.log(item);
+//   });
+
+// array.reverse().forEach(function (item) {
+//     console.log(item);
+//   });
+
+// array.sort().forEach(function (item) {
+// console.log(item);
+// });
+
+// const car = {
+// brand: "Fiat",
+// model: "500",
+// year: 50,
+// };
+
+// let { brand, model, year } = car;
+
+// console.log(brand, model, year);
+
+let rnd = Math.random() * 555;
+console.log(`Nahodne cislo je: ${rnd}`)
